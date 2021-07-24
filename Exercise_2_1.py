ea0da36faba3df74c75a2154e65e280b04600d4d
thienan2050 <thienan2050>
2021-07-24 17:45:57 +0700

    Add Trapezium'rule integration

--- Exercise_2_1.py
+++ Exercise_2_1.py
@@ -28,5 +28,33 @@ def Simpson(function, a, b, N):
         s += function(a+k*h)
     return h*s
 
+def Trapezium(function, a, b, N):
+    '''
+    Parameters
+    ----------
+    function : function of a single variable
+    a , b    : interval of integration [a,b]
+    N        : number of subintervals of [a,b]
+
+    Returns
+    -------
+    Approximation of the integral of f(x) from a to b using the
+        Trapezium rule with N subintervals of equal length.
+
+    Examples
+    --------
+    >>> Trapezium(lambda x: x**4 - 2*x + 1, 0.0, 2.0, 10)
+    4.50656
+    >>> Trapezium(lambda x: x**4 - 2*x + 1, 0.0, 2.0, 1000)
+    4.4000106666656
+    '''
+    x = np.linspace(a, b, N+1)
+    y = function(x)
+    y_right = y[1:] # right endpoints
+    y_left = y[:-1] # left endpoints
+    dx = (b - a)/N
+    T = (dx/2) * np.sum(y_right + y_left)
+    return T
+
 
 
