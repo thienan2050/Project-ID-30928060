04f1bf450220db31744101018d8269c6cf8af011
thienan2050 <thienan2050>
2021-07-28 01:25:37 +0700

    Done Exercise 3

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,38 +1,42 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
 
-import matplotlib.pyplot as plt
-from random import *
-from numpy import *
-import sys
+def LotkaVolteraModel(x, alpha, beta, gamma, delta):
+    return np.array([alpha*x[0] - beta*x[0]*x[1], delta*x[0]*x[1] - gamma*x[1]])
 
-# model parameters
-a = 1
-b = 0.5
-c = 2
-d = 0.5
-dt = 0.0001
-limitTime = 30
+def RungeKutta4(f, x0, t0, tf, dt):
+    t = np.arange(t0, tf, dt)
+    nt = t.size
+    nx = x0.size 
+    x = np.zeros((nx, nt))
+    x[:, 0] = x0
+    for k in range(nt - 1):
+        k1 = dt*f(t[k], x[:,k])
+        k2 = dt*f(t[k] + dt/2, x[:,k] + k1/2)
+        k3 = dt*f(t[k] + dt/2, x[:,k] + k2/2)
+        k4 = dt*f(t[k] + dt, x[:,k] + k3)
+        dx = (k1 + 2*k2 + 2*k3 +k4)/6
+        x[:, k+1] = x[:, k] + dx
+    return x, t
 
-# initial time and populations
-t = 0
-x = 2
-y = 2
+f = lambda t, x : LotkaVolteraModel(x, 1.1, 0.4, 0.4, 0.1)
+x0 = np.array([20, 5])
+t0 = 0
+tf = 30
+dt = 0.001
 
-t_list = []
-x_list = []
-y_list = []
+x, t = RungeKutta4(f, x0, t0, tf, dt)
 
-# initialize lists
-t_list.append(t)
-x_list.append(x)
-y_list.append(y)
+plt.subplot(1, 2, 1)
+plt.plot(t, x[0,:], 'r', label = 'Preys')
+plt.plot(t, x[1,:], 'g', label = 'Predators')
+plt.xlabel('Time (t)')
+plt.grid()
+plt.legend()
 
-while t < limitTime:
-    t = t + dt
-    x = x + (a*x - b*x*y)*dt
-    y = y + (-c*y + d*x*y)*dt
-    t_list.append(t)
-    x_list.append(x)
-    y_list.append(y)
-
-plt.plot(t_list, x_list, 'g', t_list, y_list, 'b', linewidth = 3)
+plt.subplot(1, 2, 2)
+plt.plot(x[0,:], x[1,:])
+plt.xlabel('Preys')
+plt.ylabel('Predators')
+plt.grid()
 plt.show()
