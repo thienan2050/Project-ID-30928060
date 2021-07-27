f4845e4a47aad34f43644cd84983b2df2d6b6084
thienan2050 <thienan2050>
2021-07-27 20:37:48 +0700

    Exercise 2.4 : Missing b)

---
+++ Exercise_2_4.py
@@ -0,0 +1,28 @@
+from scipy.integrate import quad
+from numpy import sqrt, sin, cos, pi
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+def Bessel(theta, m, x):
+    return (1/pi)*(cos(m*theta - x*sin(theta)))
+def J(m, x):
+    return quad(Bessel, 0, pi, args=(m, x))[0]
+
+#print J(1.0, 5.0)
+
+# a)
+x = np.linspace(0, 20, 100)
+J0 = [J(0, _) for _ in x]
+J1 = [J(1, _) for _ in x]
+J2 = [J(2, _) for _ in x]
+#plt.xlim(0, 20)
+#plt.plot(x, J0)
+#plt.plot(x, J1)
+#plt.plot(x, J2)
+#plt.show()
+
+# b)
+def Intensity(lamda, r):
+    return (J(1, (2*pi*r)/lamda)/((2*pi*r)/lamda))**2
+
