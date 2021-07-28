289ca8cec146026f8b6adb4475447dda2e496279
thienan2050 <thienan2050>
2021-07-28 09:34:11 +0700

    Combine code for Exercise 3

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,6 +1,7 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 
+# a) Solve equations
 def LotkaVolteraModel(x, alpha, beta, gamma, delta):
     return np.array([alpha*x[0] - beta*x[0]*x[1], delta*x[0]*x[1] - gamma*x[1]])
 
@@ -28,15 +29,53 @@ dt = 0.001
 x, t = RungeKutta4(f, x0, t0, tf, dt)
 
 plt.subplot(1, 2, 1)
-plt.plot(t, x[0,:], 'r', label = 'rabbits')
-plt.plot(t, x[1,:], 'g', label = 'foxes')
-plt.xlabel('Time (t)')
-plt.grid()
-plt.legend()
-
-plt.subplot(1, 2, 2)
 plt.plot(x[0,:], x[1,:])
 plt.xlabel('rabbits')
 plt.ylabel('foxes')
 plt.grid()
+
+
+
+# b) Graph 
+from random import *
+from numpy import *
+import sys
+
+# model parameters
+a = 1
+b = 0.5
+c = 2
+d = 0.5
+dt = 0.0001
+limitTime = 30
+
+# initial time and populations
+t = 0
+x = 2
+y = 2
+
+t_list = []
+x_list = []
+y_list = []
+
+# initialize lists
+t_list.append(t)
+x_list.append(x)
+y_list.append(y)
+
+while t < limitTime:
+    t = t + dt
+    x = x + (a*x - b*x*y)*dt
+    y = y + (-c*y + d*x*y)*dt
+    t_list.append(t)
+    x_list.append(x)
+    y_list.append(y)
+
+plt.subplot(1, 2, 2)
+plt.plot(t_list, x_list, 'r', label = 'rabbits')
+plt.plot(t_list, y_list, 'g', label = 'foxes')
+plt.xlabel('Time (t)')
+plt.xlim(0, 30)
+plt.grid()
+plt.legend()
 plt.show()
